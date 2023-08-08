8a1c7e20e28c7f0380012160583d3706c40c5085
Krishna Mano <krishna.d.mano@gmail.com>
2023-08-07 21:46:57 -0400

    created plan to solve day 2 and added information to txt file

---
+++ day2/day2.py
@@ -0,0 +1,11 @@
+day2 = open("day2.txt", "r")
+
+# Recognize what A, B, C, X, Y, and Z are representing (rock, paper, or scissors and opponent vs. you)
+
+# Set equations to calculate who wins which round
+
+# Set equations to calculate how many points are gained by each player per round
+
+# Add up all the rounds and calculate the final number
+
+day2.close()
